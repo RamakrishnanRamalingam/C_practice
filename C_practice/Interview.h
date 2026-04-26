932a96a137973ad499f2c2c4b72aa8a5ea411ab7
Ramakrishnan Ramalingam <r.ramkieee@gmail.com>
2026-04-26 16:11:02 -0400

    added new file and splited main to interview file

---
+++ C_practice/Interview.h
@@ -0,0 +1,10 @@
+//
+//  Interview.h
+//  C_practice
+//
+//  Created by Rama Thean on 2026-04-26.
+//
+#import <Cocoa/Cocoa.h>
+#include <math.h>
+
+void interview_function(void);
--- C_practice/Linkedlist.h
+++ C_practice/Linkedlist.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+
 //
 //  Linkedlist.h
 //  C_practice
